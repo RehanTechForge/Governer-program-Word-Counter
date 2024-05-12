8ce51ca7d79ecd5d17329e6f91814061633eefb4
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-05-13 04:17:18 +0500

    word counter project

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.green("Welcome To Word Count App"));
+const promptColor = chalk.blue;
+const secntence = await inquirer.prompt([{
+        type: "input",
+        name: "name",
+        message: promptColor("Enter Your Sentence"),
+    }]);
+const ans = secntence.name.trim().split(" ");
+console.log(chalk.green(`Word Count Is: ${chalk.red(ans.length)}`));
